0bec04a1072f54f7b69f811a5ae3a6b9655ddf67
Najah-hawa <najah.hawa@email.com>
2026-06-14 22:52:36 +0300

    Configure Google Analytics for Huda Taddabur

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// قمنا بإضافة importProvidersFrom في نهاية السطر الأول هنا
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),  // Här ger vi routerkonfigurationen
-    provideHttpClient()  // HTTP-klienten
+    provideHttpClient(),  // HTTP-klienten
+    importProvidersFrom(
+      NgxGoogleAnalyticsModule.forRoot('G-TQE5Z130WM'),
+      NgxGoogleAnalyticsRouterModule
+    )
   ]
 };
