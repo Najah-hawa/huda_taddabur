6ba1914a4b052481831710fd5c451c783837ba3f
Najah-hawa <najah.hawa@email.com>
2026-06-18 13:09:05 +0300

    fixat PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
 // قمنا بإضافة importProvidersFrom في نهاية السطر الأول هنا
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),  // Här ger vi routerkonfigurationen
-    provideHttpClient()  // HTTP-klienten
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })  // HTTP-klienten
   
   ]
 };
